0d9fec23328073d6f5f29c792ed37ca13fce9382
Abhiram Annaluru <annaluru@wisc.edu>
2024-03-14 01:02:10 -0700

    chure

---
+++ chure.sh
@@ -0,0 +1,10 @@
+rand_num=$((RANDOM % 26))
+
+rand_let=$(echo $((rand_num + 97)) | awk '{printf "%c", $1}')
+
+echo $rand_let >> file.txt
+
+git add .
+git commit -m "chure"
+git push -u origin main
+

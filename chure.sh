0d639ed19a0b7d60432f6ddef51661fcb7adab7b
Abhiram Annaluru <annaluru@wisc.edu>
2024-04-15 14:48:57 -0500

    chure8

--- chure.sh
+++ chure.sh
@@ -5,6 +5,6 @@ rand_let=$(echo $((rand_num + 97)) | awk '{printf "%c", $1}')
 echo $rand_let >> file.txt
 
 git add .
-git commit -m "chure"
+git commit -m "chure${rand_num}"
 git push -u origin main
 

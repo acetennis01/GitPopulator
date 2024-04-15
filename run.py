204c1ad9216930fbf396af541ad9dbd7629a4357
Abhiram Annaluru <annaluru@wisc.edu>
2024-04-15 14:44:22 -0500

    chure

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import subprocess
+import os
 
 # Define the path to your bash script
-script_path = '/Users/abhianna/Documents/garbage/gitShit/chure.sh'
+script_path = './chure.sh'
 
+os.system(script_path)
+'''
 try:
     # Run the bash script
     result = subprocess.run([script_path], capture_output=True, text=True, check=True)
@@ -14,3 +17,5 @@ except FileNotFoundError:
     print(f"The script {script_path} does not exist.")
 except Exception as e:
     print("An unexpected error occurred:", str(e))
+'''
+
